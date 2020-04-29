94444277552fb8634457a6b2d6bb80461450761a
brby2549 <55068210+brby2549@users.noreply.github.com>
2020-04-28 21:49:30 -0600

    Add files via upload

---
+++ HashLinProb.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+#include "Hash.hpp"
+using namespace std;
+
+HashTable::HashTable(int bsize)
+{
+    tableSize = bsize;
+    table = new node*[tableSize];
+
+}
+
+bool HashTable::insertItem(int key)
+{
+    unsigned int hashIndex = hashFunction(key);
+    
+
+   while (table[hashIndex] != NULL && numOfcolision < tableSize)
+    {        
+        numOfcolision++;
+        hashIndex = (hashIndex + 1) % tableSize;
+    }
+    table[hashIndex] = createNode(key, NULL);
+    numOfcolision = 0;
+    return true;
+}
+
+unsigned int HashTable::hashFunction(int key)
+{
+    //calculate the hash, check for collision, perform collision resolution if needed
+    return (key % tableSize);
+}
+
+void HashTable::printTable()
+{
+    for(int i = 1; i < tableSize; i++)
+    {
+        if(table[i] != NULL)
+        cout << table[i]->key << " ";
+        else
+        {
+            cout<< "Table[" << i << "] is NULL ";
+        }
+        
+    }
+    cout << endl;   
+}
+
+node* HashTable::searchItem(int key)
+{
+    int hashIndex = hashFunction(key);
+    int count = 0;
+    while (table[hashIndex] != NULL)
+    {      
+        if(count == tableSize){
+            break;
+        }  
+        if(table[hashIndex]->key = key){
+            return table[hashIndex];
+        }
+
+        hashIndex = (hashIndex + 1) % tableSize;
+        count++;
+    }
+    return NULL;
+}
