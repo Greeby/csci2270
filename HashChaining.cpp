93fb9eb903107187776a31e1f7c04708c7898c5c
Greeby <wgreeby@gmail.com>
2020-04-28 20:46:00 -0500

    Add files via upload

--- HashChaining.cpp
+++ HashChaining.cpp
@@ -1,5 +1,5 @@
+#include "HashTest.hpp"
 #include<iostream>
-#include "Hash.hpp";
 using namespace std;
 
 HashTable::HashTable(int bsize)
@@ -9,7 +9,13 @@ HashTable::HashTable(int bsize)
 
 bool HashTable::insertItem(int key)
 {
-    table[hashFunction(key)]->key = key;
+    node* currNode = table[hashFunction(key)];
+    while(currNode->next != NULL)
+    {
+        currNode = currNode->next;
+    }
+    currNode->next = createNode(key, NULL);
+    return true;
 }
 
 unsigned int HashTable::hashFunction(int key)
@@ -17,61 +23,43 @@ unsigned int HashTable::hashFunction(int key)
     //calculate the hash, check for collision, perform collision resolution if needed
     int hash = key % tableSize;
 
-    //check if there is a collision
-    if(table[hash] == 0)
-    {
-        return hash;
-    }
-    //otherwise commense the collision resloution
-    else
-    {
-        numOfcolision++;
-        //First try a linear probe starting from the collision point
-        for(int i = hash + 1; i < tableSize; i++)
-        {
-            //check for open spot
-            if(table[i] == 0)
-            {
-                return i;
-            }
-            //check if we are at the end of the list and reset if we are
-            if(i == tableSize-1)
-            {
-                i = -1;
-            }
-            //check if we are back at our original hash and break if we did
-            if(i == hash)
-            {
-                break;
-            }  
-        }
-    }  
+    return hash;
 }
 
 void HashTable::printTable()
 {
-    node * currNode = table[0];
-    while(currNode->next)
+    node* currNode;
+    for(int i = 0; i < tableSize; i++)
     {
-        cout << currNode->key << " ";
+        cout << table[0] << " ";
+        //print the list at index
+        currNode = table[i];
+        while(currNode)
+        {
+            cout << currNode->key << " ";
+            currNode = currNode ->next;
+        }
     }
-    cout << endl;
+    cout << endl;   
 }
 
 node* HashTable::searchItem(int key)
 {
-    for (int i = 0; i < tableSize; i++)
-    {
-        if (table[i]->key = key)
-        {
-            return table[i];
-        }
-        else
-        {
-            //check the chain
-        }
-        
-    }   
+    node * currNode; 
+ 
+    currNode = table[hashFunction(key)];
+    //loop through the LL
+    while (currNode)
+       {
+           if(currNode->key == key)
+           {
+               return currNode;
+           }
+           else
+           {
+               currNode = currNode->next;
+           }    
+        }   
     return NULL;
 }
 
