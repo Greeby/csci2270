e5e7edd154aa1be0445502d1d7d8f6d7804465a0
Greeby <wgreeby@gmail.com>
2020-04-28 20:42:57 -0500

    Add files via upload

--- HashLinProbe.cpp
+++ HashLinProbe.cpp
@@ -9,56 +9,26 @@ HashTable::HashTable(int bsize)
 
 bool HashTable::insertItem(int key)
 {
-    node *newNode = createNode(key, NULL);
-    table[hashFunction(key)] = newNode;
+    int hashIndex = hashFunction(key);
+    while (table[hashIndex] != NULL)
+    {
+        hashIndex = (hashIndex + 1) % tableSize;
+    }
+    table[hashIndex] = createNode(key, NULL);
+    return true;
 }
 
 unsigned int HashTable::hashFunction(int key)
 {
     //calculate the hash, check for collision, perform collision resolution if needed
     int hash = key % tableSize;
-
-    //check if there is a collision
-    if(table[hash] == 0)
-    {
-        return hash;
-    }
-    //otherwise commense the collision resloution
-    else
-    {
-        //if the hash value is the last index we will increment back one for our loop
-        if(hash == tableSize - 1)
-        {
-            hash = hash - 1;
-        }
-        //linear probe starting from the collision point
-        for(int i = hash + 1; i < tableSize; i++)
-        {
-            //check for open spot
-            if(table[i] == NULL)
-            {
-                return i;
-            }
-            //check if we are at the end of the list and reset if we are
-            if(i == tableSize-1)
-            {
-                i = -1;
-            }
-            //check if we are back at our original hash and break if we did
-            if(i == hash)
-            {
-                //The list is already full
-                break;
-            }  
-        }
-    }  
 }
 
 void HashTable::printTable()
 {
     for(int i = 0; i < tableSize; i++)
     {
-        cout << table[0] << " ";
+        cout << table[0]->key << " ";
     }
     cout << endl;   
 }
